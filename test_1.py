147995af3526f0c1524d822ac3048394a4e6a9e0
Anton Baglaev <abaglaev@yandex.ru>
2024-12-16 15:57:45 +0300

    page object

--- test_1.py
+++ test_1.py
@@ -1,22 +1,11 @@
-import yaml
-from module import Site
+from testpage import OperationsHelper
+import logging
 
-with open("testdata.yaml") as f:
-    testdata = yaml.safe_load(f)
-site = Site(testdata["address"])
-
-def test_step1():
-    x_selector1 = """//*[@id='login']/div[1]/label/input"""
-    input1 = site.find_element("xpath", x_selector1)
-    input1.send_keys("test")
-    x_selector2 = """//*[@id='login']/div[2]/label/input"""
-    input2 = site.find_element("xpath", x_selector2)
-    input2.send_keys("test")
-
-test_step1()
-
-# css_selector = "span.mdc-text-field__ripple"
-# print(site.get_element_property("css", css_selector, "height"))
-#
-# xpath = '//*[@id="login"]/div[3]/button/div'
-# print(site.get_element_property("xpath", xpath, "color"))
+def test_step1(browser):
+    logging.info("Test1 Starting")
+    testpage = OperationsHelper(browser)
+    testpage.go_to_site()
+    testpage.enter_login("test")
+    testpage.enter_pass("test")
+    testpage.click_login_button()
+    # assert testpage.get_error_text() == "401"
